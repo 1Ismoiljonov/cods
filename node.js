4ab38896ccb286a56209ad1e01892f3a03cc742a
1Ismoiljonov <99031003+1Ismoiljonov@users.noreply.github.com>
2023-01-25 17:43:19 +0500

    test

--- node.js
+++ node.js
@@ -1,1 +1,13 @@
-prompt
+// let names = 'samandar'
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i].toUpperCase(names[0]));
+// }
+
+// let surName = 'samandar' ;
+// console.log(surName);
+
+
+let names = 'samandar'
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
